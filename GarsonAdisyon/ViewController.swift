d151c3d3d23647a032ef1b6ee1fbb0462c912cf1
İbrahim Özçelik <ibrahimozcelik1@yandex.com>
2018-01-10 11:39:13 +0300

    Initial Commit

---
+++ GarsonAdisyon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GarsonAdisyon
+//
+//  Created by Air on 10.01.2018.
+//  Copyright © 2018 Air. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
